69869f6d54eea5020fa446f0a18ca2628901fe49
yorklicode <118232602+yorklicode@users.noreply.github.com>
2023-09-25 20:37:06 -0600

    week2Assignment

---
+++ app/StudentInfo/page.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+    return(
+        <main className="assignment2">
+        <p>Name: Ting Yau Li York</p>
+        <p>Course section: CPRG 306 C</p>
+        <Link href="https://github.com/yorklicode/cprg306-assignments">
+        https://github.com/yorklicode/cprg306-assignments</Link>
+        </main>
+    )
+}
---
+++ app/week2/page.js
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+import React from 'react';
+import './page.css';
+
+export default function Week2() {
+    return(
+        <main className="week2">
+        <div className="container">
+        <div className="left">
+        <h1>My Shopping List</h1>
+        </div>
+       
+       <div className="right">
+        <p>Name: Ting Yau Li York</p>
+        <p>Course section: CPRG 306 C</p>
+        <Link href="https://github.com/yorklicode/cprg306-assignments">
+        https://github.com/yorklicode/cprg306-assignments</Link>
+    </div></div>
+        </main>
+    )
+}
