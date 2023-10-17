e5437eed5154523a5c108749bfe9bc8c51af3f65
yorklicode <118232602+yorklicode@users.noreply.github.com>
2023-10-17 00:06:37 -0600

    week5 optional extra challenge

--- app/week5/item-list.js
+++ app/week5/item-list.js
@@ -7,33 +7,80 @@ import items from './items.json';
 function ItemList() {
     const [sortBy, setSortBy] = useState("name");
 
-    const sortedItems = [...items].sort((a, b) => {
-      if (sortBy === "name") return a.name.localeCompare(b.name);
-      if (sortBy === "category") return a.category.localeCompare(b.category);
-      return 0;
-    });
+    // Group items by category
+  const groupedItems = items.reduce((acc, item) => {
+    if (!acc[item.category]) {
+      acc[item.category] = [];
+    }
+    acc[item.category].push(item);
+    return acc;
+  }, {});
+
+  // Sort the categories
+  const sortedCategories = Object.keys(groupedItems).sort();
+
+   // Sort the items within each category
+   sortedCategories.forEach(category => {
+    groupedItems[category].sort((a, b) => a.name.localeCompare(b.name));
+  });
+
+
+  
+
+     const sortedItems = [...items].sort((a, b) => {
+       if (sortBy === "name") return a.name.localeCompare(b.name);
+       if (sortBy === "category") return a.category.localeCompare(b.category);
+       return 0;
+     });
   
     return (
       <div>
+         <h1>Shopping List</h1>
         <button
-          className={sortBy === "name" ? "bg-blue-500" : ""}
+          className={`m-2 p-2 ${sortBy === "name" ? "bg-blue-300" : ""}`}
           onClick={() => setSortBy("name")}
         >
           Sort by Name
         </button>
         <button
-          className={sortBy === "category" ? "bg-blue-500" : ""}
-          onClick={() => setSortBy("category")}
+           className={`m-2 p-2 ${sortBy === "category" ? "bg-blue-300" : ""}`}
+           onClick={() => setSortBy("category")}
         >
           Sort by Category
         </button>
+
+        <button 
+        className={`m-2 p-2 ${sortBy === "groupedByCategory" ? "bg-blue-300" : ""}`}
+        onClick={() => setSortBy("groupedByCategory")}
+      >
+        Group by Category
+      </button>
+        
+
+
+        {sortBy === "groupedByCategory" ? (
+        <div>
+          {sortedCategories.map(category => (
+            <div key={category}>
+              <h2 className="capitalize">{category}</h2>
+              <ul>
+                {groupedItems[category].map(item => (
+                  <Item key={item.id} {...item} />
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      ) : (
         <ul>
           {sortedItems.map(item => (
             <Item key={item.id} {...item} />
           ))}
         </ul>
-      </div>
-    );
+      )}
+
+    </div>
+  );
 }
 
 export default ItemList;
